de716e2b51c6322a8089f2bd7a7b21ab4201f676
Lucas Ferreira <lucasferreiraek@gmail.com>
2024-10-04 02:46:18 -0300

    WIP: navigate using pagination

--- main.go
+++ main.go
@@ -19,59 +19,68 @@ func main() {
 		panic(err)
 	}
 
-	defer res.Body.Close()
-
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 
 	if err != nil {
 		panic(err)
 	}
 
+	res.Body.Close()
+
 	pageLinks := getPages(doc)
 
 	fmt.Println("NÚMERO DE PÁGINAS:", len(pageLinks))
 	fmt.Println("PRÓXIMOS LINKS DA PAGINAÇÃO:", pageLinks)
 
-	links := doc.Find(".showindex__children").Find("a").Map(func(i int, s *goquery.Selection) string {
-		link, _ := s.Attr("href")
+	for page, link := range pageLinks {
+		fmt.Printf("ACESSANDO PÁGINA %v - link: %v", page+1, link)
 
-		return link
-	})
-
-	titles := doc.Find(".showindex__children").Find("a").Map(func(i int, s *goquery.Selection) string {
-		title, _ := s.Attr("title")
-
-		return title
-	})
-
-	fmt.Println(titles)
-	fmt.Println("LINKS DE TODOS OS MODELOS ENCONTRADOS:", links)
-
-	for i, link := range links {
-		fmt.Printf("ACESSANDO MODELO NÚMERO %v: %v\n", i+1, link)
-
-		fmt.Println()
-		jerseyRes, err := http.Get(mainUrl + link)
-
+		response, err := http.Get(link)
 		if err != nil {
-			fmt.Println(err)
-			continue
+			panic(err)
 		}
 
-		defer jerseyRes.Body.Close()
+		defer response.Body.Close()
 
-		jerseyDoc, err := goquery.NewDocumentFromReader(jerseyRes.Body)
+		modelLinks := doc.Find(".showindex__children").Find("a").Map(func(i int, s *goquery.Selection) string {
+			link, _ := s.Attr("href")
 
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		jerseyDoc.Find(".showalbum__children").Each(func(i int, s *goquery.Selection) {
-			id, _ := s.Attr("data-id")
-			fmt.Println("EXIBINDO OS LINKS DAS IMAGENS DO MODELO DA CAMISA")
-			fmt.Println(mainUrl + id + "?uid=1")
+			return link
 		})
+
+		// modelTitles := doc.Find(".showindex__children").Find("a").Map(func(i int, s *goquery.Selection) string {
+		// 	title, _ := s.Attr("title")
+
+		// 	return title
+		// })
+
+		for i, link := range modelLinks {
+			fmt.Printf("ACESSANDO MODELO NÚMERO %v: %v\n", i+1, link)
+
+			fmt.Println()
+			jerseyRes, err := http.Get(mainUrl + link)
+
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			defer jerseyRes.Body.Close()
+
+			jerseyDoc, err := goquery.NewDocumentFromReader(jerseyRes.Body)
+
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			jerseyDoc.Find(".showalbum__children").Each(func(i int, s *goquery.Selection) {
+				id, _ := s.Attr("data-id")
+				fmt.Println("EXIBINDO OS LINKS DAS IMAGENS DO MODELO DA CAMISA")
+				fmt.Println(mainUrl + id + "?uid=1")
+			})
+
+		}
 
 	}
 
