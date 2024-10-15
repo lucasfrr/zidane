b103a6191f2f2450a872493f672b8b8974068fef
Lucas Ferreira <lucasferreiraek@gmail.com>
2024-10-15 00:13:51 -0300

    wip: mount json structure to show all links

--- cmd/main.go
+++ cmd/main.go
@@ -25,6 +25,10 @@ func main() {
 	fmt.Println("NÚMERO DE PÁGINAS:", len(pageLinks))
 	fmt.Println("PRÓXIMOS LINKS DA PAGINAÇÃO:", pageLinks)
 
+	// data := make([]map[string]string, )
+	var albums []map[string]string
+	var images []map[string]string
+
 	for i, link := range pageLinks {
 		fmt.Printf("PAGINA %v - %v\n", i+1, link)
 
@@ -36,16 +40,40 @@ func main() {
 			return link
 		})
 
+		albumTitles := pageDocument.Find(".album__main").Map(func(i int, s *goquery.Selection) string {
+			title, _ := s.Attr("title")
+
+			return title
+		})
+
 		for j, album := range albumLinks {
-			urlAlbum := "https://alisports.x.yupoo.com" + album + "?uid=1"
-			fmt.Printf("ACESSANDO ALBUM %v - %v\n", j, urlAlbum)
+			urlAlbum := "https://alisports.x.yupoo.com" + album
+			fmt.Printf("ACESSANDO ALBUM %v - %v | %v\n", j, urlAlbum, albumTitles[j])
 
 			albumDocument := handlers.MakeRequest(urlAlbum)
 
 			albumDocument.Find(".showalbum__children").Each(func(i int, s *goquery.Selection) {
 				id, _ := s.Attr("data-id")
-				fmt.Printf("ID da imagem: %v\n", id)
+				title, _ := s.Find(".text_overflow").Attr("title")
+
+				image := map[string]string{
+					"uri":   "https://alisports.x.yupoo.com/" + id + "?uid=1",
+					"title": title,
+				}
+
+				images = append(images, image)
+
+				fmt.Printf("URI da imagem: https://alisports.x.yupoo.com/%v?uid=1 | %v\n", id, title)
 			})
+
+			album := map[string]string{
+				"title":  albumTitles[j],
+				"uri":    urlAlbum,
+				"images": images,
+			}
+
+			albums = append(albums, album)
+			// data = append(data, albums)
 		}
 
 	}
